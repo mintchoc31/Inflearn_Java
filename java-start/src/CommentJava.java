15e403af59c2fc49d0b5f3120ff2b3b2be4a716d
mintchoc31 <mintchoc31@gmail.com>
2024-01-28 23:54:39 +0900

    김영한의 자바 입문 섹션0~1 완료

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
