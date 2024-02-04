d4f28c316648a21ad6c1c29fda2e77e46aba2ca7
mintchoc31 <mintchoc31@gmail.com>
2024-02-05 00:38:00 +0900

    섹션2 문제 풀이 완료

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+//  문제3. long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하는 프로그램을 작성하세요.
+//  boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화한 후, 출력하는 프로그램을 작성하세요.
+
+    long a = 10000000000L;
+    System.out.println("a = " + a);
+//  soutv : 현재 선택한 변수의 이름과 값을 출력하는 단축키
+
+    boolean b = true;
+    System.out.println("b = " + b);
+    }
+}
